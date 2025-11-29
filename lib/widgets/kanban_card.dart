e36070e238eeb8d3ae3452ec3b528860986e0eb1
motociklist <budik.leha2@gmail.com>
2025-11-29 22:55:04 +0300

    [fk-3] + add task save in Firestore

--- lib/widgets/kanban_card.dart
+++ lib/widgets/kanban_card.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../models/kanban_task.dart';
 
-typedef CardTapCallback = void Function(KanbanTask task);
+typedef CardTapCallback = Future<void> Function(KanbanTask task);
 
 class KanbanCard extends StatelessWidget {
   final KanbanTask task;
@@ -9,30 +9,49 @@ class KanbanCard extends StatelessWidget {
   final CardTapCallback? onTap;
   final Color Function(KanbanStatus, BuildContext) colorForStatus;
 
-  const KanbanCard({Key? key, required this.task, this.isDragging = false, this.onTap, required this.colorForStatus}) : super(key: key);
+  const KanbanCard(
+      {Key? key,
+      required this.task,
+      this.isDragging = false,
+      this.onTap,
+      required this.colorForStatus})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final bg = task.color ?? colorForStatus(task.status, context);
-    final textColor = ThemeData.estimateBrightnessForColor(bg) == Brightness.dark ? Colors.white : Colors.black87;
+    final textColor =
+        ThemeData.estimateBrightnessForColor(bg) == Brightness.dark
+            ? Colors.white
+            : Colors.black87;
 
     return GestureDetector(
-      onTap: () => onTap?.call(task),
+      onTap: () {
+        if (onTap != null) onTap!(task);
+      },
       child: Container(
         width: double.infinity,
         decoration: BoxDecoration(
           color: bg,
           borderRadius: BorderRadius.circular(10),
-          boxShadow: isDragging ? [const BoxShadow(color: Colors.black26, blurRadius: 8)] : [const BoxShadow(color: Colors.black12, blurRadius: 4)],
+          boxShadow: isDragging
+              ? [const BoxShadow(color: Colors.black26, blurRadius: 8)]
+              : [const BoxShadow(color: Colors.black12, blurRadius: 4)],
         ),
         padding: const EdgeInsets.all(12),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text(task.title, style: TextStyle(fontSize: 14, fontWeight: FontWeight.w600, color: textColor)),
+            Text(task.title,
+                style: TextStyle(
+                    fontSize: 14,
+                    fontWeight: FontWeight.w600,
+                    color: textColor)),
             if (task.description != null) ...[
               const SizedBox(height: 6),
-              Text(task.description!, style: TextStyle(fontSize: 13, color: textColor.withAlpha(230))),
+              Text(task.description!,
+                  style:
+                      TextStyle(fontSize: 13, color: textColor.withAlpha(230))),
             ]
           ],
         ),
